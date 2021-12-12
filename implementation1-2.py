a37d5199994b132f9e93e0ee130ba51d339d3acf
junghanlee <89134202+JungHanLeee@users.noreply.github.com>
2021-12-12 21:17:41 +0900

    Add files via upload

---
+++ implementation1-2.py
@@ -0,0 +1,9 @@
+n=int(input())
+cnt=0
+for i in range(0,n):
+    for j in range(0,60):
+        for h in range(0,60):
+            time=str(i)+str(m)+str(s)
+            if '3' in time:
+                cnt+=1
+                        
