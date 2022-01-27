94b66f7906dd7cc083fcede17cbbaf769324e4f2
junghanlee <89134202+JungHanLeee@users.noreply.github.com>
2022-01-27 17:45:50 +0900

    Delete K번째 수.py

--- programmers/Level1/K번째 수.py
+++
@@ -1,9 +0,0 @@
-def solution(array, commands):
-    cnt = len(commands)
-    result = []
-
-    for i in range(len(commands)):
-        arr = array[commands[i][0] - 1:commands[i][1]]
-        arr = sorted(arr)
-        result.append(arr[commands[i][2] - 1])
-    return result
