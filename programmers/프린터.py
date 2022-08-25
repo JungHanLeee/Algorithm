c961de55baeb4b36b5a72df17f47d8cd2a9f5edf
junghanlee <dlwjdgks126@hansung.ac.kr>
2022-08-25 23:37:58 +0900

    Create 프린터.py

---
+++ programmers/프린터.py
@@ -0,0 +1,21 @@
+https://school.programmers.co.kr/learn/courses/30/lessons/42587
+  
+def solution(priorities, location):
+  answer = 0
+  cnt=0
+  while(len(priorities)!=0): 
+      if location==0:
+          if(priorities[0]<max(priorities)):
+              priorities.append(priorities.pop(0))
+              location=len(priorities)-1
+          else:
+              return cnt+1
+      else:
+          if priorities[0]<max(priorities):
+              priorities.append(priorities.pop(0))
+              location-=1
+          else:
+              priorities.pop(0)
+              location-=1
+              cnt+=1
+  return cnt
